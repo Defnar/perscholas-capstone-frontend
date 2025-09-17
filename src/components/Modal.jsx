b38fc0fbe13b39f4890bc6037541058ead13b34c
Dennis <dennis-farris@hotmail.com>
2025-09-17 11:21:29 -0400

    created modal component

---
+++ src/components/Modal.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useRef, useState } from "react";
+
+export default function Modal({ children }) {
+  const [modalOpen, setModalOpen] = useState(false);
+  const modalRef = useRef(null);
+
+  //open modal on mount
+  useEffect(() => {
+    setModalOpen(true);
+  }, []);
+
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        setModalOpen(false);
+      }
+    };
+
+    const handleEscapeKey = (event) => {
+      if (modalRef.current) {
+        if (event.key === "Escape") setModalOpen(false);
+      }
+    };
+
+    if (modalOpen) {
+      document.addEventListener("mousedown", handleOutsideClick());
+      document.addEventListener("keydown", handleEscapeKey());
+    } else {
+      document.removeEventListener("mousedown", handleOutsideClick);
+      document.removeEventListener("keydown", handleEscapeKey());
+    }
+  }, [modalOpen]);
+
+  return <div>{modalOpen && <div ref={modalRef}>{children}</div>}</div>;
+}
