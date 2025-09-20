3885082627fdb9504c78126390f9792e640f29b0
Dennis <dennis-farris@hotmail.com>
2025-09-20 15:33:09 -0400

    edit project button created and functional.  deadline now shows on project screen

--- src/pages/SingleProjectPage.jsx
+++ src/pages/SingleProjectPage.jsx
@@ -6,6 +6,7 @@ import { useParams } from "react-router-dom";
 import Modal from "../components/Modal";
 import EditTask from "../components/EditTask";
 import Collaborators from "../components/Collaborators";
+import ProjectEdit from "../components/ProjectEdit";
 
 export default function SingleProjectPage() {
   const { user } = useContext(AuthContext);
@@ -14,6 +15,7 @@ export default function SingleProjectPage() {
   const [visibleCount, setVisibleCount] = useState(null);
   const [permissions, setPermissions] = useState(null);
   const [projectData, setProjectData] = useState(null);
+  const [editProjectModal, setEditProjectModal] = useState(null);
   const { projectId } = useParams();
 
   const [data, loading, error] = useFetch(`projects/${projectId}`);
@@ -55,18 +57,30 @@ export default function SingleProjectPage() {
     setTaskModalOpen((prev) => !prev);
   };
 
+  const toggleProjectModal = () => {
+    setEditProjectModal((prev) => !prev);
+  };
   return (
     <>
       {!user && <h2>Unauthorized, Please Log In</h2>}
       {user && projectData && (
-        <div className="flex flex-col gap-4 h-screen">
+        <div className="flex flex-col gap-4 h-screen p-2">
           <div className="flex gap-4">
-            <div className="flex-1 border border-gray-200 shadow-md p-4 rounded-lg bg-white">
+            <div className="flex flex-col gap-2 flex-1 border border-gray-200 shadow-md p-4 rounded-lg">
               <h2 className="text-4xl font-bold mb-2">{projectData.title}</h2>
-              <section>
+              <p>
                 <span className="font-semibold">About:</span>{" "}
                 {projectData.description}
-              </section>
+              </p>
+              <p><span className="font-semibold">Status: </span>{projectData.status}</p>
+              {projectData?.deadline && <p><span className="font-semibold">Deadline: </span>{new Date(projectData.deadline).toLocaleString()}</p>}
+              <div className="flex flex-1 h-full w-full">
+                {permissions?.includes("editProject") && (
+                  <button className="self-end bg-emerald-200 px-4 py-2 rounded-md shadow-md hover:bg-emerald-300 hover:cursor-pointer" onClick={toggleProjectModal}>
+                    Edit
+                  </button>
+                )}
+              </div>
             </div>
 
             <div className="w-1/3 border border-gray-200 shadow-md p-4 rounded-lg bg-white">
@@ -127,6 +141,18 @@ export default function SingleProjectPage() {
             closeModal={toggleTaskModal}
             projectId={projectId}
             setTasks={setTasks}
+          />
+        </Modal>
+      )}
+      {editProjectModal && (
+        <Modal modalOpen={editProjectModal} setModalOpen={setEditProjectModal}>
+          <ProjectEdit
+            closeModal={toggleProjectModal}
+            title={projectData.title}
+            description={projectData.description}
+            _id={projectData._id}
+            status={projectData.status}
+            privateProject={projectData.privateProject}
           />
         </Modal>
       )}
