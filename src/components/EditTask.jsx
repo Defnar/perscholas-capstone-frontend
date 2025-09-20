e51325059c1fa9a52f1bcba424936b3d76ec05d6
Dennis <dennis-farris@hotmail.com>
2025-09-20 13:55:09 -0400

    styled the task add/edit screen

--- src/components/EditTask.jsx
+++ src/components/EditTask.jsx
@@ -77,36 +77,58 @@ export default function EditTask({
     }
   };
 
+  const inputStyles = "flex-1 shadow-md border border-gray-200 h-7 bg-gray-100";
+
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="title">Title: </label>
-      <input
-        type="text"
-        name="title"
-        onChange={handleChange}
-        value={taskInfo.title}
-      />
-      <label htmlFor="description">Description</label>
-      <textarea
-        name="description"
-        onChange={handleChange}
-        value={taskInfo.description}
-      />
+    <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="title">Title: </label>
+        <input
+          className={inputStyles}
+          type="text"
+          name="title"
+          onChange={handleChange}
+          value={taskInfo.title}
+        />
+      </div>
+      <div className="flex flex-col content-center">
+        <label htmlFor="description">Description</label>
+
+        <textarea
+          name="description"
+          className={`${inputStyles} max-h-full`}
+          onChange={handleChange}
+          value={taskInfo.description}
+        />
+      </div>
       <label htmlFor="status">Status: </label>
-      <select name="status" value={taskInfo.status} onChange={handleChange}>
+      <select
+        name="status"
+        className={inputStyles}
+        value={taskInfo.status}
+        onChange={handleChange}
+      >
         {statusDropdown}
       </select>
       <label htmlFor="deadline">Deadline: </label>
       <input
+        className={inputStyles}
         type="datetime-local"
         name="deadline"
         value={taskInfo.deadline}
         onChange={handleChange}
       />
-      <button type="submit">Submit</button>
-      <button type="button" onClick={handleCancel}>
-        Cancel
-      </button>
+      <div className="flex flex-row justify-evenly">
+        <button
+          type="submit"
+          className="bg-emerald-200 hover:bg-emerald-300 px-4 py-2 rounded-md shadow-md"
+        >
+          Submit
+        </button>
+        <button type="button" className="bg-gray-200 hover:bg-gray-300 px-4 py-2 rounded-md shadow-md" onClick={handleCancel}>
+          Cancel
+        </button>
+      </div>
     </form>
   );
 }
